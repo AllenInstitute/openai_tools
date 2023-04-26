d131ae39e57919a57ea7058c003da32a616eef20
Jerome Lecoq <jlecoq@gmail.com>
2023-04-25 22:43:32 -0700

    Add doi parser"

--- src/papers_extractor/doi_parser.py
+++ src/papers_extractor/doi_parser.py
@@ -1,2 +1,74 @@
 # This file contains classes to handle DOI links and extract the
 # publication text from them.
+from habanero import Crossref
+import requests
+from bs4 import BeautifulSoup
+
+class DoiParser:
+    """This class is used to handle DOI links and extract the
+    publication metadata from them.
+    """
+
+    def __init__(self, doi):
+        """Initializes the class with the DOI link.
+            args:
+                doi: This is the DOI link that will be used to extract
+                publication text.
+        """
+        self.doi = doi
+        self.metadata = None
+
+    def get_doi_metatdata(self):
+        if self.metadata:
+            return self.metadata
+        else:
+            cr = Crossref()
+            self.metadata = cr.works(ids=self.doi)
+            return self.metadata
+
+    def get_title(self):
+        """This function extracts the title from the metadata."""
+        metatdata = self.get_doi_metatdata()
+        title = metatdata['message']['title'][0]
+        return title
+    
+    def get_abstract(self):
+        """This function extracts the abstract from the metadata."""
+        metatdata = self.get_doi_metatdata()
+        abstract = metatdata['message']['abstract']
+        return abstract
+    
+    def get_authors(self):
+        """This function extracts the authors from the metadata."""
+        metatdata = self.get_doi_metatdata()
+        authors = metatdata['message']['author']
+        return authors
+    
+    def get_pdf_link(self):
+        """This function extracts the pdf link from the metadata."""
+        metatdata = self.get_doi_metatdata()
+
+        # If published is biorxiv, then we use the 
+        # biorxiv api to get the pdf link
+        if metatdata['message']['publisher'] == 'Cold Spring Harbor Laboratory':
+            url = 'https://www.biorxiv.org/content/' + self.doi
+
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                pdf_link = soup.find('a', class_='article-dl-pdf-link')['href']
+                return f"https://www.biorxiv.org{pdf_link}"
+            else:
+                print(f"Error: Unable to fetch biorxiv webpage. Status code {response.status_code}")
+                return None
+        else:
+            full_text_links = metatdata['message'].get('link', [])
+            for link in full_text_links:
+                print(link)
+                content_type = link.get('content-type', '')
+                if content_type == 'application/pdf':
+                    pdf_url = link['URL']
+                    return pdf_url
+            else:
+                print("No pdf link found")
+                return None
---
+++ tests/test_doi_parser.py
@@ -0,0 +1,29 @@
+from papers_extractor.doi_parser import DoiParser
+
+def test_nature_doi_pdf():
+    doi_parser = DoiParser('110.1038/nature12373')
+    assert doi_parser.get_pdf_link() == 'http://www.nature.com/articles/nature12373.pdf'
+
+def test_biorxiv1_doi_pdf():
+    doi_parser = DoiParser('10.1101/2020.03.03.972133')
+    assert doi_parser.get_pdf_link() == 'https://www.biorxiv.org/content/10.1101/2020.03.03.972133v1.full.pdf'
+
+def test_biorxiv2_doi_pdf():
+    doi_parser = DoiParser('10.1101/2020.10.15.341602')
+    assert doi_parser.get_pdf_link() == 'https://www.biorxiv.org/content/10.1101/2020.10.15.341602v2.full.pdf'
+
+def test_cell_doi_pdf():
+    doi_parser = DoiParser('10.1016/j.celrep.2023.112434')
+    assert doi_parser.get_pdf_link() == None
+
+def test_title_doi_pdf():
+    doi_parser = DoiParser('10.1101/2020.03.03.972133')
+    assert doi_parser.get_title() == 'AI-aided design of novel targeted covalent inhibitors against SARS-CoV-2'
+
+def test_abstract_doi():
+    doi_parser = DoiParser('10.1101/2020.03.03.972133')
+    assert doi_parser.get_abstract()[0:10] == '<jats:titl'
+
+def test_author_doi():
+    doi_parser = DoiParser('10.1101/2020.03.03.972133')
+    assert doi_parser.get_authors()[0]['given'] == 'Bowen'
