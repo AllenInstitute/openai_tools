ec6509a2f2b743103d60ef309427fe3d854ca1a0
Jerome Lecoq <jlecoq@gmail.com>
2023-05-14 19:04:32 -0700

    add dot parameter to adjust plot based on citation

--- scripts/pubmed_embedding.py
+++ scripts/pubmed_embedding.py
@@ -54,7 +54,9 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--add_citation_count",
-        help="Whether to add the citation count to the plot",
+        help="Whether to change the size of the points based on the citation \
+            count. This will make the plot more readable but will take more \
+            time to run.",
         type=bool,
         default=False,
     )
@@ -109,4 +111,7 @@ if __name__ == "__main__":
     multi_paper.plot_paper_embedding_map(save_path=save_path,
                                          field=args.field,
                                          perplexity=args.perplexity,
+                                         add_citation_count=(
+                                            args.add_citation_count
+                                         ),
                                          label='xshort')
